a21846e2739302cf5dd961475559c14e2a304d96
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-09-05 12:01:43 -0700

    swiperjs init and editing. Working on FAQ ux

--- client/src/components/FAQPhoto/index.js
+++ client/src/components/FAQPhoto/index.js
@@ -1,36 +1,57 @@
 import React, { useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Autoplay, Mousewheel, Lazy, EffectFade } from "swiper";
 import "./styles.css";
 import "swiper/swiper.scss";
-// Import Swiper styles
+import "swiper/components/navigation/navigation.scss";
+import "swiper/components/pagination/pagination.scss";
+import "swiper/components/scrollbar/scrollbar.scss";
+import image from "./images/hops.jpg";
+import image1 from "./images/hops1.jpg";
+import image2 from "./images/hops2.jpg";
+import image3 from "./images/hops3.jpg";
+import image4 from "./images/hops4.jpg";
+import image5 from "./images/hops5.jpg";
+import image6 from "./images/hops6.jpg";
 
 function FAQPhoto() {
-  useEffect(() => {
-    var swiper = new Swiper(".swiper-container", {
-      direction: "vertical",
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
-  }, []);
+  SwiperCore.use([Autoplay, Lazy, Mousewheel, EffectFade]);
   return (
-    <div class="swiper-container">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide">Slide 1</div>
-        <div class="swiper-slide">Slide 2</div>
-        <div class="swiper-slide">Slide 3</div>
-        <div class="swiper-slide">Slide 4</div>
-        <div class="swiper-slide">Slide 5</div>
-        <div class="swiper-slide">Slide 6</div>
-        <div class="swiper-slide">Slide 7</div>
-        <div class="swiper-slide">Slide 8</div>
-        <div class="swiper-slide">Slide 9</div>
-        <div class="swiper-slide">Slide 10</div>
-      </div>
-
-      <div class="swiper-pagination"></div>
-    </div>
+    <Swiper
+      slidesPerView={1}
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      autoplay={{
+        delay: 500,
+        disableOnInteraction: false,
+      }}
+      Lazy={true}
+      Mousewheel={true}
+      EffectFade={true}
+      direction={"vertical"}
+    >
+      <SwiperSlide>
+        <img className="materialboxed faq-photo" src={image} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="materialboxed faq-photo" src={image1} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="materialboxed faq-photo" src={image2} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="materialboxed faq-photo" src={image3} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="materialboxed faq-photo" src={image4} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="materialboxed faq-photo" src={image5} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="materialboxed faq-photo" src={image6} />
+      </SwiperSlide>
+    </Swiper>
   );
 }
 
